c786a14713b0018cbe090ad13aa3cfc0fd3b936a
jhartatx <jhartatx@gmail.com>
2017-12-20 14:32:27 -0600

    fixed form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,4 +2,21 @@ $(".navT").on("click", function(){
   $(this).toggleClass("active");
   $("#menu").toggleClass("open");
   $(".content").toggleClass("shift");
-})
+});
+
+function submitMessage(){
+    event.preventDefault();
+    $.ajax({
+        url: "https://formspree.io/jhartatx@gmail.com",
+        method: "POST",
+        data: {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#message').val()
+        },
+        dataType: 'json'
+    }).done(function(){
+        $('#name, #email, #message').val('');
+        $('#thankYou, #contactSubmit').toggle('slow');
+    })
+}
